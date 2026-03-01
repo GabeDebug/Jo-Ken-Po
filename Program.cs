607c51cfc21ef0d4efb45e1d9052afbdcf9a8af2
GabrielDebug <gabrielmessiasc0d3@gmail.com>
2026-03-01 06:05:38 -0300

    Criação das opção

--- Program.cs
+++ Program.cs
@@ -14,7 +14,13 @@ namespace Jo_Ken_PO
         {
             Console.Clear();
 
-            
+            Console.WriteLine("Escolha as opção: ");
+            Console.WriteLine($"1 - Pedra ");
+            Console.WriteLine("2 - Papel :");
+            Console.WriteLine("3 - Tisoura :");
+            Console.WriteLine("4 - Sair :");
+
+            short v1 = short.Parse(Console.ReadLine());
         }
     }
 }
