640e2856bb437e9f10c2d99abeb8b37c36d32092
GabrielDebug <gabrielmessiasc0d3@gmail.com>
2026-03-01 06:02:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Jo_Ken_PO
+{
+    enum Escolhas
+    {
+        Pedra = 1,
+        Papel = 2,
+        Tisoura = 3
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            
+        }
+    }
+}
